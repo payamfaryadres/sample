6e642e42efdc76425b9cf1affcd6ebd72efbbbff
payam <payam.ff@gmail.com>
2020-04-14 23:36:00 +0430

    change Prime algorithm checker in order to work efficiently

--- prime/PrimeChecker.java
+++ prime/PrimeChecker.java
@@ -8,36 +8,36 @@ import java.util.stream.Stream;
 
 public class PrimeChecker {
 
+
     public static boolean isPrime(int i) {
         if (i <= 3)
-        return true;
-        else if (i % 2 == 0 || i % 3 == 0 ) {
-            return false;
-        } else {
-            long sqrti = (long) Math.sqrt(i) + 1;
-            for (long j = 6L; i<= sqrti;i=+6 ){
-                if(i%(j-1)==0 || i%(j+1)==0)
-                    return false;
-            }
-        }
-        return true;
+            return true;
+        else if ((i * i - 1) % 24 == 0)
+            return true;
+        return false;
     }
-
 
     public static void main(String[] args) {
         Path path = Paths.get(args[0]);
+        //Path path = Paths.get("/home/payam/Projects/Test_for_check_code/src/main/resources/primeTest.txt");
+        StringBuilder builder = new StringBuilder();
         try (Stream<String> lines = Files.lines(path)) {
             lines.forEach(s -> {
                 if (isPrime(Integer.parseInt(s))) {
-                    System.out.println(1);
+                    builder.append('1');
+
 
                 } else {
-                    System.out.println(0);
+                    builder.append('0');
+
                 }
+                builder.append('\n');
             });
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(builder.toString());
+
     }
 }
