8bbeca3e828c24273dc25b813a1ebc7a3acac374
payam <payam.ff@gmail.com>
2020-04-15 19:07:53 +0430

    change Prime algorithm

--- prime/PrimeChecker.java
+++ prime/PrimeChecker.java
@@ -2,25 +2,29 @@
 
 import java.util.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class PrimeChecker {
     public static void main(String[] args) {
         Scanner sc = null;
         try {
-            sc = new Scanner(new File(args[0]));
+
+            final BufferedReader in = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(
+                            args[0]), StandardCharsets.UTF_8));
+            String line;
             OutputStream out = new BufferedOutputStream(System.out);
-            while (sc.hasNext()) {
-                String num = sc.nextLine();
-                out.write((isPrime(Integer.parseInt(num)) + "\n").getBytes());
+            while ((line = in.readLine()) != null) {
+                out.write((isPrime(Integer.parseInt(line)) + "\n").getBytes());
             }
             out.flush();
+            in.close();
         } catch (Exception ex) {
         }
     }
 
     private static int isPrime(int num) {
-        if (num <= 3 || num == 5 || num == 7) return 1;
-        if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0) return 0;
+        if (num <= 3) return 1;
         int max = (int) Math.sqrt(num) + 1;
         for (int i = 3; i <= max; i += 2) {
             if (num % i == 0) {
