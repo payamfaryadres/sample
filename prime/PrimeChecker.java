f21b831b4265c9aa2a23b2e3febb7a20e135c091
payam <payam.ff@gmail.com>
2020-04-15 15:51:50 +0430

    change Prime algorithm checker in order to work correctly

--- prime/PrimeChecker.java
+++ prime/PrimeChecker.java
@@ -1,41 +1,42 @@
 
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.nio.charset.StandardCharsets;
+import java.io.*;
 public class PrimeChecker {
 
 
-    public static char isPrime(long input_number) {
-        if (input_number <= 3) return '1';
-        if (input_number % 2 == 0) return '0';
+    static final String ZERO="0\n";
+    static final String ONE="1\n";
+    public static String isPrime(long input_number) {
+        if (input_number <= 3) return ONE;
+        if (input_number % 2 == 0) return ZERO;
         int max = (int) Math.sqrt(input_number) + 1;
-        for (int i = 3; i <= max; i+=2) {
+        for (int i = 3; i <= max; i += 2) {
             if (input_number % i == 0) {
-                return '0';
+                return ZERO;
             }
         }
-        return '1';
+        return ONE;
     }
 
     public static void main(String[] args) {
         StringBuilder builder = new StringBuilder();
         try {
+            OutputStream out = new BufferedOutputStream ( System.out );
             final BufferedReader in = new BufferedReader(
                     new InputStreamReader(new FileInputStream(args[0]), StandardCharsets.UTF_8));
             String line;
             while ((line = in.readLine()) != null) {
-                builder.append(isPrime(Long.parseLong(line)));
-                builder.append('\n');
+                out.write((isPrime(Integer.parseInt(line))).getBytes());
             }
             in.close();
-        } catch (final IOException e) {
-            e.printStackTrace();
+            out.flush();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
 
-        System.out.println(builder.toString());
+        System.out.print(builder.toString());
 
     }
 }
