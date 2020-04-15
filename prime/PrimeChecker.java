714b92751543146cc46acf577a42ca9499a29599
payam <payam.ff@gmail.com>
2020-04-15 17:24:07 +0430

    change Prime algorithm checker in order to work correctly

--- prime/PrimeChecker.java
+++ prime/PrimeChecker.java
@@ -1,42 +1,34 @@
 
 
-
-import java.nio.charset.StandardCharsets;
+import java.util.*;
 import java.io.*;
 public class PrimeChecker {
-
-
-    static final String ZERO="0\n";
-    static final String ONE="1\n";
-    public static String isPrime(long input_number) {
-        if (input_number <= 3) return ONE;
-        if (input_number % 2 == 0) return ZERO;
-        int max = (int) Math.sqrt(input_number) + 1;
-        for (int i = 3; i <= max; i += 2) {
-            if (input_number % i == 0) {
-                return ZERO;
+    public static void main(String[] args) {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File(args[0]));
+            OutputStream out = new BufferedOutputStream ( System.out );
+            while(sc.hasNext()) {
+                String num = sc.nextLine();
+                out.write((isPrime(Integer.parseInt(num)) + "\n").getBytes());
             }
+            out.flush();
         }
-        return ONE;
+        catch(Exception ex) {}
     }
 
-    public static void main(String[] args) {
-        StringBuilder builder = new StringBuilder();
-        try {
-            OutputStream out = new BufferedOutputStream ( System.out );
-            final BufferedReader in = new BufferedReader(
-                    new InputStreamReader(new FileInputStream(args[0]), StandardCharsets.UTF_8));
-            String line;
-            while ((line = in.readLine()) != null) {
-                out.write((isPrime(Integer.parseInt(line))).getBytes());
+    private static int isPrime(int num) {
+        if (num <= 3) return 1;
+        if (num % 2 == 0) return 0;
+        int max = (int) Math.sqrt(num) + 1;
+        for (int i = 3; i <= max; i+=2) {
+            if (num % i == 0) {
+                return 0;
             }
-            in.close();
-            out.flush();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
         }
-
-        System.out.print(builder.toString());
-
+        return 1;
     }
 }
+
+
+
