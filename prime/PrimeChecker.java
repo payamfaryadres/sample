69b10833ec81b45c24cf0008a69af58650276633
payam <payam.ff@gmail.com>
2020-04-15 17:53:04 +0430

    add some validation

--- prime/PrimeChecker.java
+++ prime/PrimeChecker.java
@@ -2,26 +2,27 @@
 
 import java.util.*;
 import java.io.*;
+
 public class PrimeChecker {
     public static void main(String[] args) {
         Scanner sc = null;
         try {
             sc = new Scanner(new File(args[0]));
-            OutputStream out = new BufferedOutputStream ( System.out );
-            while(sc.hasNext()) {
+            OutputStream out = new BufferedOutputStream(System.out);
+            while (sc.hasNext()) {
                 String num = sc.nextLine();
                 out.write((isPrime(Integer.parseInt(num)) + "\n").getBytes());
             }
             out.flush();
+        } catch (Exception ex) {
         }
-        catch(Exception ex) {}
     }
 
     private static int isPrime(int num) {
-        if (num <= 3) return 1;
-        if (num % 2 == 0) return 0;
+        if (num <= 3 || num == 5 || num == 7) return 1;
+        if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0) return 0;
         int max = (int) Math.sqrt(num) + 1;
-        for (int i = 3; i <= max; i+=2) {
+        for (int i = 3; i <= max; i += 2) {
             if (num % i == 0) {
                 return 0;
             }
