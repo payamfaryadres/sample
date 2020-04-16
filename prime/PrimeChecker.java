d726fdbf6a688108f093c85dd4a1e379467821b3
payam <payam.ff@gmail.com>
2020-04-16 12:58:32 +0430

    change convert strategy :|

--- prime/PrimeChecker.java
+++ prime/PrimeChecker.java
@@ -5,17 +5,17 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class PrimeChecker {
+    public static final  String ZERO = "0\n";
+    public static final  String ONE = "1\n";
     public static void main(String[] args) {
-        Scanner sc = null;
         try {
-
             final BufferedReader in = new BufferedReader(
                     new InputStreamReader(new FileInputStream(
-                            args[0]), StandardCharsets.UTF_8));
+                            args[0]), StandardCharsets.US_ASCII));
             String line;
             OutputStream out = new BufferedOutputStream(System.out);
             while ((line = in.readLine()) != null) {
-                out.write((isPrime(Integer.parseInt(line)) + "\n").getBytes());
+                out.write((isPrime(convertStringToInt(line)) ).getBytes());
             }
             out.flush();
             in.close();
@@ -23,15 +23,24 @@ public class PrimeChecker {
         }
     }
 
-    private static int isPrime(int num) {
-        if (num <= 3) return 1;
+    private static String isPrime(int num) {
+        if (num <= 3) return ONE;
         int max = (int) Math.sqrt(num) + 1;
         for (int i = 3; i <= max; i += 2) {
             if (num % i == 0) {
-                return 0;
+                return ZERO;
             }
         }
-        return 1;
+        return ONE;
+    }
+
+    private static int convertStringToInt(String number){
+        int res = 0;
+        int len = number.length();
+        for (int i = 0; i < len; i++) {
+            res = res * 10 + number.charAt(i) - '0';
+        }
+        return res;
     }
 }
 
