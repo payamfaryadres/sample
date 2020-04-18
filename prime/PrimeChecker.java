ee64205426cc29ebb41a3ce623574864cc0dcfa8
payam <payam.ff@gmail.com>
2020-04-18 15:31:24 +0430

    change Prime algorithm

--- prime/PrimeChecker.java
+++ prime/PrimeChecker.java
@@ -1,13 +1,8 @@
-
-
-import java.util.*;
-import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class PrimeChecker {
-    public static final String ZERO = "0\n";
-    public static final String ONE = "1\n";
-
+    public static final  String ZERO = "0\n";
+    public static final  String ONE = "1\n";
     public static void main(String[] args) {
         try {
             final BufferedReader in = new BufferedReader(
@@ -16,7 +11,7 @@ public class PrimeChecker {
             String line;
             OutputStream out = new BufferedOutputStream(System.out);
             while ((line = in.readLine()) != null) {
-                out.write((isPrime(Integer.parseInt(line))).getBytes());
+                out.write((isPrime(convertToInt(line)) ).getBytes());
             }
             out.flush();
             in.close();
@@ -25,11 +20,25 @@ public class PrimeChecker {
     }
 
     private static String isPrime(int num) {
-        if (num <= 3 || num == 5) return ONE;
-        if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0) return ZERO;
+        if (num <= 3) return ONE;
+        int max = (int) Math.sqrt(num) + 1;
+        for (int i = 3; i <= max; i += 2) {
+            if (num % i == 0) {
+                return ZERO;
+            }
+        }
         return ONE;
     }
-
+    private static int convertToInt(String data) {
+        int num = 0;
+        int i = 0;
+        int len = data.length();
+        while (len > i) {
+            num *= 10;
+            num += data.charAt(i++) - '0';
+        }
+        return num;
+    }
 
 }
 
