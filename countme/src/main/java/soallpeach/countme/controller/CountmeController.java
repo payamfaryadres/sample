577443f0360ea96899a6c4a5f98637d94f431623
payam <payam.ff@gmail.com>
2020-05-03 00:16:41 +0430

    create the first version of countme

--- countme/src/main/java/soallpeach/countme/controller/CountmeController.java
+++ countme/src/main/java/soallpeach/countme/controller/CountmeController.java
@@ -1,7 +1,32 @@
 package soallpeach.countme.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 public class CountmeController {
+
+    Set<Long> numbers = new HashSet<>();
+    @PostMapping
+    public ResponseEntity sumNumber(@RequestBody String request){
+         addNumberToSet(Long.valueOf(request.substring(0,request.length()-1)));
+        return ResponseEntity.ok().build();
+    }
+
+    protected void addNumberToSet(Long number){
+        numbers.add(number);
+    }
+
+    @RequestMapping(path = "/count")
+    public ResponseEntity getCount(){
+        return ResponseEntity.ok(numbers.parallelStream().mapToLong(Long::longValue).sum());
+    }
+
 }
